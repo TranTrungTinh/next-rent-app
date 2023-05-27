11e95b4caf593972481e7e7214b0d539eec99353
TinhTran <trantrungtinh4954@gmail.com>
2023-05-27 15:12:35 +0700

    feat: :white_check_mark: Implement register with email/password

---
+++ app/api/register/route.ts
@@ -0,0 +1,17 @@
+import prismaDb from '@/app/libs/prismadb.ts'
+import bcrypt from 'bcrypt'
+import { NextResponse } from 'next/server'
+
+export async function POST(req: Request) {
+  const body = await req.json()
+
+  const { email, name, password } = body
+
+  const hashedPassword = await bcrypt.hash(password, 12)
+
+  const user = await prismaDb.user.create({
+    data: { email, name, hashedPassword }
+  })
+
+  return NextResponse.json(user)
+}
